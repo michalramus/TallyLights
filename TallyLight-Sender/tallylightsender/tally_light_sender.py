9a829e204ef29c465e6e305dd79f566814fa91d9
Michal Ramus <ramus.michal21@gmail.com>
2024-05-28 12:47:29 +0200

    ref[script]: Split functions into smaller and avoid global variables
    Class OBS created to avoid global variables. Used class properties
    instead. Some functionalities of main moved into OBS class.

--- TallyLight-Sender/tallylightsender/tally_light_sender.py
+++ TallyLight-Sender/tallylightsender/tally_light_sender.py
@@ -1,4 +1,6 @@
-import obsws_python as obs
+import obsws_python as obsws
+
+# from obsws_python.error import OBSSDKTimeoutError
 from tally_controller import tally_light, tally_light_enums
 import time
 
@@ -7,113 +9,120 @@ port = 4455
 password = "123456"
 timeout = 3
 
-cr: obs.ReqClient
-ce: obs.EventClient
 
-global tallies
-tallies: list[tuple[str, tally_light.Tally]]  # [Scene name, Tally]
+class OBS:
 
-global previewTally
-previewTally: tuple[str, tally_light.Tally] | None = None  # Scene on preview
-global programTally
-programTally: tuple[str, tally_light.Tally] | None = None  # Scene on program
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        password: str,
+        conn_timeout: int,
+        tallies: list[tuple[str, tally_light.Tally]],
+    ) -> None:
+        self._host = host
+        self._port = port
+        self._password = password
+        self._conn_timeout = conn_timeout
+        self._tallies = tallies
+
+        self.connect()
+
+    def connect(self) -> None:
+        print(f"Host: {self._host}:{self._port}")
+        print("Connecting...")
+
+        connected = False
+        while not connected:  # Retry until connected
+            try:
+                self._ce = obsws.EventClient(
+                    host=self._host,
+                    port=self._port,
+                    password=self._password,
+                    timeout=self._conn_timeout,
+                )
+                self._cr = obsws.ReqClient(
+                    host=host, port=port, password=password, timeout=timeout
+                )
+
+            except Exception:
+                time.sleep(timeout)
+            else:
+                connected = True
+
+        # Register callbacks
+        self._ce.callback.register(self.on_scene_transition_started)
+        self._ce.callback.register(self.on_scene_transition_ended)
+        # Update preview and program tally variables
+        self._preview_tally = self._get_current_preview_tally()
+        self._program_tally = self._get_current_program_tally()
+        print("Connected 😄")
+
+    def _get_current_program_tally(self) -> tuple[str, tally_light.Tally] | None:
+        resp = self._cr.get_current_program_scene()
+        scene_name = resp.scene_name
+
+        for tally in self._tallies:
+            if tally[0] == scene_name:
+                return tally
+        return None
+
+    def _get_current_preview_tally(self) -> tuple[str, tally_light.Tally] | None:
+        resp = self._cr.get_current_preview_scene()
+        scene_name = resp.scene_name
+
+        for tally in self._tallies:
+            if tally[0] == scene_name:
+                return tally
+        return None
+
+    def on_scene_transition_started(self, data):
+        print("Transition stared")
+
+        tally = self._get_current_program_tally()
+        if tally is not None:
+            tally[1].set_color(tally_light_enums.Color.RED)
+
+    def on_scene_transition_ended(self, data):
+        print("Transition ended")
+
+        tally = self._get_current_program_tally()
+        if tally is not None:
+            # Third scene goes to program; Second goes from program to preview; First scene is turned off from preview
+            if (self._preview_tally is not None) and (
+                tally[0] != self._preview_tally[0]
+            ):
+                self._preview_tally[1].set_color(tally_light_enums.Color.OFF)
+
+            # Swap program and preview
+            self._preview_tally = self._program_tally
+            self._program_tally = tally
+            if (self._preview_tally is not None) and (
+                self._preview_tally != self._program_tally
+            ):
+                self._preview_tally[1].set_color(tally_light_enums.Color.OFF)
+
+        elif (tally is None) and (self._program_tally is not None):
+            self._program_tally[1].set_color(tally_light_enums.Color.OFF)
+            self._program_tally = None
 
 
 def main():
-    # from Tally import Tally  # I don't know why, but without it doesn't work
-
-    print(f"Host: {host}:{port}")
-    print("Connecting...")
-
-    global tallies
     tallies = [
         ("cam1", tally_light.Tally("192.168.200.11", 10373, 1)),
         ("cam2", tally_light.Tally("192.168.200.12", 10373, 2)),
         ("cam3", tally_light.Tally("192.168.200.13", 10373, 3)),
     ]
 
-    while True:  # Retry until connected
-        try:
-            global ce
-            ce = obs.EventClient(
-                host=host, port=port, password=password, timeout=timeout
-            )
-            global cr
-            cr = obs.ReqClient(host=host, port=port, password=password, timeout=timeout)
-
-        except Exception as e:
-            time.sleep(timeout)
-        else:
-            break
-
-    # Set previewTally and programTally variables
-    global programTally
-    resp = cr.get_current_program_scene()
-    scene_name = resp.scene_name
-    for tally in tallies:
-        if tally[0] == scene_name:
-            programTally = tally
-            break
-
-    global previewTally
-    resp = cr.get_current_preview_scene()
-    scene_name = resp.scene_name
-    for tally in tallies:
-        if tally[0] == scene_name:
-
-            previewTally = tally
-            break
-
-    # Register callbacks
-    ce.callback.register(on_scene_transition_started)
-    ce.callback.register(on_scene_transition_ended)
-    print("Connected 😄")
-
-
-def on_scene_transition_started(data):
-    print("Transition stared")
-    resp = cr.get_current_program_scene()
-    scene_name = resp.scene_name
-
-    for tally in tallies:
-        if tally[0] == scene_name:
-            tally[1].set_color(tally_light_enums.Color.RED)
-            break
-
-
-def on_scene_transition_ended(data):
-    print("Transition ended")
-    global previewTally
-    global programTally
-
-    resp = cr.get_current_program_scene()
-    scene_name = resp.scene_name
-    found = False
-    for tally in tallies:
-        if tally[0] == scene_name:
-            # Third scene goes to program; Second goes from program to preview; First scene is turned off from preview
-            if (previewTally is not None) and (tally[0] != previewTally[0]):
-                previewTally[1].set_color(tally_light_enums.Color.OFF)
-
-            # Swap program and preview
-            previewTally = programTally
-            programTally = tally
-            if (previewTally is not None) and (previewTally != programTally):
-                previewTally[1].set_color(tally_light_enums.Color.OFF)
-
-            found = True
-            break
-    if (not found) and (programTally is not None):
-        programTally[1].set_color(tally_light_enums.Color.OFF)
-        programTally = None
+    x = OBS(host, port, password, timeout, tallies)
 
 
 if __name__ == "__main__":
     main()
     while True:
         time.sleep(30)
-        try:
-            cr.get_current_program_scene()  # check if connected
-        except:
-            print("Disconnected")
-            main()
+        # try:
+        #     cr.get_current_program_scene()  # check if connected
+        # except OBSSDKError:
+        #     print("Disconnected")
+        #     main()
